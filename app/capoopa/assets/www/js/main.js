836c6c7ee5bf52eae57a3cdf57017294cd10c7e0
maxou <maxou.ipp@gmail.com>
2012-12-21 11:07:25 +0100

    Revert "Récupération des données json"
    This reverts commit f4c6b45fbab8270c3409aab11b806b4066c9c932.

--- app/capoopa/assets/www/js/main.js
+++ app/capoopa/assets/www/js/main.js
@@ -1,22 +1,17 @@
 $(document).ready(function(){
-  $.ajax({
-      url: 'http://localhost:8000/api/challenge/',
-      contentType: 'application/json',
-      dataType: 'jsonp',
-      cache: 'false',
-      processData: 'false',
-      type: "GET",
-      success: function(data, textStatus, jqXHR) {
-        listChallenges(data.objects, '#current-challenges');
-      }
-    });
+    /*$.getJSON("js/storage/challenges.json", function(data){
+      $.each(data, function(key, item) {
+        liItem = document.createElement('li');
+        $(liItem).appendTo('#challenges');
+        link = document.createElement('a');
+        $(link).attr('href', '#').appendTo(liItem);
+        $("<img/>").addClass('thumb').attr('src', 'img/category/' + item.category + '.png').appendTo(link);
+        $("<h3/>").addClass('ui-li-heading').text(item.title).appendTo(link);
+        $("<p/>").addClass('ui-li-desc').text(item.category).appendTo(link);
+      });
+    });*/
 
-
-    toggleMenu();
-});
-
-function toggleMenu() {
-  $('.toggle').click(function() {
+    $('.toggle').click(function() {
       if ($(this).hasClass('toggle-top')) {
         $(this).removeClass('toggle-top');
         $(this).addClass('toggle-bottom');
@@ -25,18 +20,9 @@ function toggleMenu() {
         $(this).removeClass('toggle-bottom');
         $(this).addClass('toggle-top');
       }
-      $(this).next('.toggleContent').slideToggle('slow');
-    });
-}
 
-function listChallenges(data, list) {
-  $.each(data, function(key, item) {
-    liItem = document.createElement('li');
-    $(liItem).addClass('open-right').appendTo(list);
-    link = document.createElement('a');
-    $(link).attr('href', '#').appendTo(liItem);
-    $("<img/>").addClass('thumb').attr('src', 'img/category/' + item.category + '.png').appendTo(link);
-    $("<h3/>").addClass('ui-li-heading').text(item.title).appendTo(link);
-    $("<p/>").addClass('ui-li-desc').text(item.category).appendTo(link);
-  });
-}
+      $(this).next('.toggleContent').slideToggle('slow', function() {
+        // Animation complete.
+      });
+    });
+});
