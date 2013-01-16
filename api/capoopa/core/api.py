4b3182a843b649954f048bf30dd6c0d666b31c3a
maxou <maxou.ipp@gmail.com>
2013-01-16 12:25:38 +0100

    Fix clef externe accesible
    changement des foreignkey en onetoonefield et ajout d'element dans les
    ressources de l'api pour faire correspondre le tout

--- api/capoopa/core/api.py
+++ api/capoopa/core/api.py
@@ -30,22 +30,6 @@ from tastypie.serializers import Serializer
 #
 #url /core/challenge/ : displays all challenges
 #url /core/challenge/<title> : displays all informations about this challenge
-class ChallengeResource(ModelResource):
-	#author = fields.ForeignKey(UserResource, 'user')
-	#users = fields.ForeignKey(UserResource, attribute='users', full=True, null=True)
-	class Meta:
-		queryset = Challenge.objects.all()
-		resource_name = 'challenge'
-		allowed_methods = ['get','post']
-		serializer = Serializer(formats=['xml', 'json'])
-		authorization= Authorization()
-
-	#choices are : answer, author, description, id, title
-	def prepend_urls(self):
-		return [
-			url(r"^(?P<resource_name>%s)/(?P<id>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
-		]
-
 
 class UserResource(ModelResource):
 	
@@ -60,11 +44,29 @@ class UserResource(ModelResource):
 	def prepend_urls(self):
 		return [
 			url(r"^(?P<resource_name>%s)/(?P<id>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
-		]		
+		]	
+
+class ChallengeResource(ModelResource):
+	author = fields.OneToOneField(UserResource, attribute='author' , related_name='author', full=True)
+	#users = fields.ForeignKey(UserResource, attribute='users', full=True, null=True)
+	class Meta:
+		queryset = Challenge.objects.all()
+		resource_name = 'challenge'
+		allowed_methods = ['get','post']
+		serializer = Serializer(formats=['xml', 'json'])
+		authorization= Authorization()
+		#fields=['author','description','title']
+
+	#choices are : answer, author, description, id, title
+	def prepend_urls(self):
+		return [
+			url(r"^(?P<resource_name>%s)/(?P<id>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+		]
+	
 
 class AnswerResource(ModelResource):
-	#userID = fields.ForeignKey(UserResource, 'user')
-	#challengeID = fields.ForeignKey(ChallengeResource, 'challenge')
+	userID = fields.OneToOneField(UserResource, attribute='userID' , related_name='userID', full=True)
+	challengeID = fields.OneToOneField(ChallengeResource, attribute='challengeID' , related_name='challengeID', full=True)
 	class Meta:
 		queryset = Answer.objects.all()
 		resource_name = 'answer'
@@ -76,6 +78,7 @@ class AnswerResource(ModelResource):
 		# ]
 
 		allowed_methods = ['get','post']
+		serializer = Serializer(formats=['xml', 'json'])
 		authorization= Authorization()
 
 	#Choices are : challengeID, id, status, userID
--- api/capoopa/core/models.py
+++ api/capoopa/core/models.py
@@ -15,7 +15,7 @@ class User(models.Model):
 class Challenge(models.Model):
 	title = models.CharField(max_length=20)
 	description = models.TextField()
-	author = models.ForeignKey(User)
+	author = models.OneToOneField(User)
 	#beginning = models.IntegerField(max_length=200)
 	#end = models.IntegerField(max_length=200)
 	#category = models.CharField(max_length=200) # cree un dico de differentes valus pour les enums
@@ -24,11 +24,11 @@ class Challenge(models.Model):
 	#type = models.CharField(max_length=200)
 	
 class Answer(models.Model):
-	userID = models.ForeignKey(User)
-	challengeID = models.ForeignKey(Challenge)
+	userID = models.OneToOneField(User)
+	challengeID = models.OneToOneField(Challenge)
 	status = models.CharField(max_length=10)
 	#media = models.IntegerField(max_length=200)
-	#nbAbuse = models.IntegerField(max_length=200) 
+	#nbAbuse = models.IntegerField(max_length=200)
 	
 
 
