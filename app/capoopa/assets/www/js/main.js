11fe666d497edf4a7ec66e67eede430dbf9e9a2e
maelle <malbox69@gmail.com>
2013-03-25 12:39:59 +0100

    Changement chemin local - online
    Just to do that online (test photo)

--- app/capoopa/assets/www/js/main.js
+++ app/capoopa/assets/www/js/main.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
 
 
 function postData(path, data) {
-  var fullPath = 'http://localhost:8000/api/core/' + path;
-  
+  //var fullPath = 'http://localhost:8000/api/core/' + path;
+  var fullPath = 'http://ssh.alwaysdata.com:11390/api/core/' + path;
   $.ajax({
     type: "POST",
     url: fullPath,
@@ -30,8 +30,8 @@ function postData(path, data) {
 // type : 0=none ; 1=objects ; 2=answers
 function loadData(path, template, type) {
   $.ajax({
-    url: 'http://localhost:8000/api/core/' + path,
-    //url: 'http://ssh.alwaysdata.com:11390/api/' + path,
+    //url: 'http://localhost:8000/api/core/' + path,
+    url: 'http://ssh.alwaysdata.com:11390/api/' + path,
     contentType: 'application/json',
     dataType: 'jsonp',
     cache: false,
