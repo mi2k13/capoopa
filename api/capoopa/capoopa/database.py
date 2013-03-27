e5d993c19b34c4907be422eb36b0daf8558fad5c
maelle <malbox69@gmail.com>
2013-03-27 11:02:31 +0100

    database.py online

--- api/capoopa/capoopa/database.py
+++ api/capoopa/capoopa/database.py
@@ -1,14 +1,14 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': 'capoopa',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',
-        #'NAME': 'capoopa_test',                      # Or path to database file if using sqlite3.
-        #'USER': 'capoopa',                      # Not used with sqlite3.
-        #'PASSWORD': 'azerty123',                  # Not used with sqlite3.
-        #'HOST': 'mysql2.alwaysdata.com',                      # Set to empty string for localhost. Not used with sqlite3.
+		#'NAME': 'capoopa',                      # Or path to database file if using sqlite3.
+        #'USER': 'root',                      # Not used with sqlite3.
+        #'PASSWORD': '',                  # Not used with sqlite3.
+        #'HOST': '',
+        'NAME': 'capoopa_test',                      # Or path to database file if using sqlite3.
+        'USER': 'capoopa',                      # Not used with sqlite3.
+        'PASSWORD': 'azerty123',                  # Not used with sqlite3.
+        'HOST': 'mysql2.alwaysdata.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
