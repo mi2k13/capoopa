01c8cc3141311f0329732fb6e1ccd964009b385c
maelle <malbox69@gmail.com>
2013-03-28 05:43:32 +0100

    nothing...

--- api/capoopa/capoopa/settings.py
+++ api/capoopa/capoopa/settings.py
@@ -51,8 +51,8 @@ MEDIA_URL = '/home/capoopa/images/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/images/'
-
+#STATIC_ROOT = '/images/'
+STATIC_ROOT =''
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
