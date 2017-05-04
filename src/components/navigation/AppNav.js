81a43d76436983f81a4523be8a25af8b87e3fd99
mi2k13 <smoooking.monkey@gmail.com>
2017-05-04 14:37:15 +0200

    feat(navigation): replace app tabBar's label with icons

--- src/components/navigation/AppNav.js
+++ src/components/navigation/AppNav.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import { TabNavigator } from 'react-navigation';
 //
 import ChallengesNav from './../navigation/ChallengesNav';
 import FriendsScreen from './../screens/FriendsScreen';
 import ProfileScreen from './../screens/ProfileScreen';
+import Icon from './../UI/Icon';
 //
 import Colors from './../../static/style/Colors';
 
@@ -12,6 +14,15 @@ const AppNav = TabNavigator({
     path: '/',
     navigationOptions: {
       tabBarLabel: 'Défis',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          name="trophy"
+          fill={tintColor}
+        />
+      ),
     },
   },
   Profile: {
@@ -19,6 +30,15 @@ const AppNav = TabNavigator({
     path: '/profile',
     navigationOptions: {
       tabBarLabel: 'Profil',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          name="user"
+          fill={tintColor}
+        />
+      ),
     },
   },
   Friends: {
@@ -26,6 +46,15 @@ const AppNav = TabNavigator({
     path: '/friends',
     navigationOptions: {
       tabBarLabel: 'Amis',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          name="community"
+          fill={tintColor}
+        />
+      ),
     },
   },
 }, {
@@ -33,11 +62,10 @@ const AppNav = TabNavigator({
   animationEnabled: false,
   swipeEnabled: false,
   tabBarOptions: {
+    showIcon: true,
+    showLabel: false,
     activeTintColor: Colors.brand,
     inactiveTintColor: '#333333',
-    labelStyle: {
-      fontSize: 12,
-    },
     indicatorStyle: {
       borderBottomColor: Colors.brand,
       borderBottomWidth: 1,
